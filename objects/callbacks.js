ecc314cdc1401341721b8d7664360e63e381856d
Mike Skelton <skeltonmr43@gmail.com>
2019-01-04 17:04:21 -0500

    callbacks

---
+++ objects/callbacks.js
@@ -0,0 +1,19 @@
+const work = {
+  name: "Innovu",
+  coworkers: ['Brandon', 'Terry', 'Dan', 'Matt', 'Scott']
+}
+
+console.log(work.coworkers);
+
+
+const cheeses = [{name: 'bree'}, {name: 'cheddar'}, {name: 'american'}]
+
+console.log(cheeses[0]);
+
+for(let key in cheeses){
+  console.log(Object.keys(work));
+}
+
+for(let key in work){
+  console.log(work[key])
+}
