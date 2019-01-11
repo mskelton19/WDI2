948462d0d5f6f22b56a19a0093c513638a1d97ec
Mike Skelton <skeltonmr43@gmail.com>
2019-01-11 16:49:32 -0500

    Game

---
+++ bootstrap/blog.js
@@ -0,0 +1,11 @@
+// $('button').on('click', () => {
+//   console.log('Click is working')
+// })
+
+$(() => {
+  console.log('window loaded')
+})
+
+$('button').click(function() {
+  alert("Handler for .click")
+})
